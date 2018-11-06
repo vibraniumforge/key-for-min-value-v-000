4d4343ac977b30a77f14d2f1817cd4b487443bfc
Greg Morgan <gregm9999@gmail.com>
2018-11-06 02:18:05 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
       min_value=value
     end
   end
-  # min_value
-  binding.pry
+  min_value
+  # binding.pry
 end
 key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})
