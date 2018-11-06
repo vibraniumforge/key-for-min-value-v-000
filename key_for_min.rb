9cef5a5bf22ac36a8b4759b9ccf804862d6d3d84
Greg Morgan <gregm9999@gmail.com>
2018-11-06 02:21:11 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,17 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require "pry"
 def key_for_min_value(name_hash)
-
+  min_value=100000000000000
+  the_key=nil
+  name_hash.each do |key, value|
+    # binding.pry
+    if value<min_value
+      min_value=value
+      the_key=key
+    end
+  end
+  the_key
+  # binding.pry
 end
+key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})
